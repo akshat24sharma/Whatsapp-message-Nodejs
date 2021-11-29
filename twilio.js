d353d8d137bcadf2d74a87127c8ab24e18035598
Akshat Sharma <34333411+akshat24sharma@users.noreply.github.com>
2021-11-29 12:47:17 +0530

    Update and rename twilo.js to twilio.js
    Removed Credentials

--- twilio.js
+++ twilio.js
@@ -8,15 +8,15 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.get('/twilio', (req, res) => {
-    const accountSid = 'AC90a20a4aaad06a3de94cd0a2735913d2';
-    const authToken = '6a36a1c801f698a98518203de697ff19';
+    const accountSid = 'Your AccountSID';
+    const authToken = 'Your Auth Token';
     const client = require('twilio')(accountSid, authToken);
 
     client.messages
         .create({
             body: 'Hello Akshat it is a msg from node app ',
-            from: 'whatsapp:+14155238886',
-            to: 'whatsapp:+919717680054'
+            from: 'whatsapp:twilio registerd number', //this is the numbers provided to me by the free twilio account, you have to register for free
+            to: 'whatsapp:Your Number'
         })
         .then(message => console.log(message.sid))
         .done();
